f0b335e54f5b0f797c18d7e2e7423d0f8c160269
Funya-okina <funyafj@gmail.com>
2025-09-05 21:59:11 +0900

    postの動確用処理作成

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import axios from 'axios';
+import multer from 'multer';
 
 const app = express();
 app.use(express.json());
+
+const upload = multer({ storage: multer.memoryStorage() }); // メモリ上に保存
 
 app.get('/landmarkData', async (req, res) => {
   const gpsInfo = req.query.lat && req.query.lon
@@ -26,6 +29,19 @@ app.get('/landmarkData', async (req, res) => {
   }
 });
 
+app.post(``, upload.fields([
+  { name: 'images', maxCount: 10 },
+  { name: `detailJson`, maxCount: 1 }
+]), async (req, res) => {
+  try {
+    const requestFiles = req.files;
+    console.log('Received files:', requestFiles);
+    res.status(200).json({ imageName: requestFiles.images.map(file => file.originalname), detailJson: requestFiles.detailJson ? requestFiles.detailJson[0].buffer.toString() : null });
+} catch (error) { 
+    console.error('Error processing data:', error);
+    res.status(500).send('Bad Request: Error processing data');
+  }
+});
 
 const port = parseInt(process.env.PORT) || 8080;
 app.listen(port, () => {
